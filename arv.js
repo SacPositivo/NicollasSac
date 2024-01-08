08ac3e39583abb47a88624c09b166ce7068632bc
SacPositivo <148489209+SacPositivo@users.noreply.github.com>
2024-01-08 17:02:26 -0300

    pr1

--- arv.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const arvoreNatal = document.getElementById("arvore-natal");
-    arvoreNatal.style.animationPlayState = "running";
-  });
-  
